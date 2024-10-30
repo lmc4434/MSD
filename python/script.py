91465107610741cdb28b76961582837067e8c953
lmc4434 <78935496+lmc4434@users.noreply.github.com>
2024-10-30 15:10:39 -0400

    Update script.py

--- python/script.py
+++ python/script.py
@@ -1,1 +1,18 @@
-#here
+import serial
+import time
+
+
+ser = serial.Serial('COM7', 9600, timeout=1)
+
+time.sleep(2)
+
+while True:
+    if ser.in_waiting > 0:
+
+        line = ser.readline().decode('utf-8').strip()
+        print(f"Received: {line}")
+
+        if line == "Hello World":
+            data = 50
+            ser.write(data.to_bytes(1, byteorder='little'))
+            print(f"Sent: {data}")
