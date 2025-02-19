45c73226dcec582e9a87cae4d6f526951657310f
MatthewSmickle <31512911+MatthewSmickle@users.noreply.github.com>
2025-02-19 14:03:51 -0500

    Update gpio.c

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -111,12 +111,13 @@ void MX_GPIO_Init(void)
 	  GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
 	  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-	  /*Configure GPIO pins : PC2 PC3 */
-	  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
+	  /*Configure GPIO pins : PC2 PC3 PC4 */
+   	  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4;
 	  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
 	  GPIO_InitStruct.Pull = GPIO_NOPULL;
-	  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+ 	  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 	  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
 }
 
 int check_button1(void)
