c69bdf915689df91dd6c776f489bc34ebb7775f7
MatthewSmickle <31512911+MatthewSmickle@users.noreply.github.com>
2025-04-16 14:00:09 -0400

    Update gpio.c

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -155,8 +155,8 @@ void MX_GPIO_Init(void)
  	  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
  	  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-	  /*Configure GPIO pin : PC2 PC3 */
-	  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
+	  /*Configure GPIO pin : PC7 */
+	  GPIO_InitStruct.Pin = GPIO_PIN_7;
 	  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
 	  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
 	  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
